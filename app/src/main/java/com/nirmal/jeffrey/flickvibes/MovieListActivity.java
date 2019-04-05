e57de95c2cc97b91cade1438212155628036b6af
Nirmal Jeffrey <jeffattry@gmail.com>
2019-04-05 21:03:15 +0530

    feat: Initial Commit

---
+++ app/src/main/java/com/nirmal/jeffrey/flickvibes/MovieListActivity.java
@@ -0,0 +1,13 @@
+package com.nirmal.jeffrey.flickvibes;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MovieListActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_list_movie);
+  }
+}
